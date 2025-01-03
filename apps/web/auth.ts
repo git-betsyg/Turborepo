b61a9bf6beb1d9abe05a5fa1d75c84c51b4b402b
chenminggui <c13558889425@icloud.com>
2025-01-03 15:31:56 +0800

    feat(next-auth): 加入身份验证

---
+++ apps/web/auth.ts
@@ -0,0 +1,97 @@
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+import { ZodError } from "zod";
+import Credentials from "next-auth/providers/credentials";
+import { signInSchema } from "./lib/zod";
+import type { Provider } from "next-auth/providers";
+import "next-auth/jwt";
+
+const providers: Provider[] = [
+  Credentials({
+    // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+    // e.g. domain, username, password, 2FA token, etc.
+    credentials: {
+      email: {},
+      password: {},
+    },
+    authorize: async (credentials) => {
+      try {
+        let user = null;
+
+        const { email, password } = await signInSchema.parseAsync(credentials);
+
+        // todo
+        // logic to salt and hash password
+        // const pwHash = await bcrypt.hash(password, 10);
+
+        // todo
+        // logic to verify if the user exists
+        // user = await getUserFromDb(email, pwHash)
+
+        if (!user) {
+          // No user found, so this is their first attempt to login
+          // Optionally, this is also the place you could do a user registration
+          throw new Error("Invalid credentials.");
+        }
+
+        // return user object with their profile data
+        return user;
+      } catch (error) {
+        if (error instanceof ZodError) {
+          // Return `null` to indicate that the credentials are invalid
+          return null;
+        }
+      }
+    },
+  }),
+  GitHub,
+];
+
+export const providerMap = providers
+  .map((provider) => {
+    if (typeof provider === "function") {
+      const providerData = provider();
+      return { id: providerData.id, name: providerData.name };
+    } else {
+      return { id: provider.id, name: provider.name };
+    }
+  })
+  .filter((provider) => provider.id !== "credentials");
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers,
+  pages: {
+    signIn: "/signin",
+    error: "/error",
+  },
+  callbacks: {
+    authorized: async ({ auth }) => {
+      // Logged in users are authenticated, otherwise redirect to login page
+      return !!auth;
+    },
+    jwt({ token, trigger, session, account }) {
+      if (trigger === "update") token.name = session.user.name;
+      if (account?.provider === "github") {
+        return { ...token, accessToken: account.access_token };
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token?.accessToken) session.accessToken = token.accessToken;
+
+      return session;
+    },
+  },
+});
+
+declare module "next-auth" {
+  interface Session {
+    accessToken?: string;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    accessToken?: string;
+  }
+}
