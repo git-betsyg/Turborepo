dd3f1e8967e1b8f97abea37b4d6a0064783fb9bb
chenminggui <c13558889425@icloud.com>
2025-01-09 15:17:50 +0800

    feat(package.json): 优化包捆绑

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
+import withBundleAnalyzer from '@next/bundle-analyzer'
 
 const nextConfig: NextConfig = {
     /* config options here */
+    // 优化包导入
+    experimental: {
+        optimizePackageImports: [],
+    },
+    // 选择特定软件包不捆绑
+    serverExternalPackages: [],
 };
 
-export default nextConfig;
+const withBundleAnalyzerConfig = withBundleAnalyzer({
+    enabled: process.env.ANALYZE === 'true',
+})
+
+export default withBundleAnalyzerConfig(nextConfig)
