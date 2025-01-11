bde9263147f39a90805ce14142ab1c0ceb475cca
chenminggui <c13558889425@icloud.com>
2025-01-11 18:01:03 +0800

    feat(middleware): 新增受保护Routes

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -2,6 +2,9 @@ import { auth } from "@/auth";
 import { getLocale, locales } from "./lib/locale";
 import { NextResponse } from "next/server";
 
+// Specify protected routes
+const protectedRoutes = ['/dashboard']
+
 // 使用 auth 方法作为包装器，此种方式会覆盖 NextAuth 的 authorized 鉴权逻辑
 export default auth((req) => {
   // Check if there is any supported locale in the pathname
@@ -10,6 +13,7 @@ export default auth((req) => {
       (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`,
   );
   const locale = getLocale(req);
+  const isProtectedRoute = protectedRoutes.includes(pathnameHasLocale?pathname.replace(`/${locale}`,''):pathname)
 
   if (!pathnameHasLocale) {
     // Redirect if there is no locale
@@ -19,10 +23,12 @@ export default auth((req) => {
     return NextResponse.redirect(req.nextUrl);
   }
 
-  if (!req.auth && req.nextUrl.pathname !== `/${locale}/signin`) {
+  if (isProtectedRoute&&!req.auth) {
     const newUrl = new URL(`/${locale}/signin`, req.nextUrl.origin)
     return NextResponse.redirect(newUrl)
   }
+
+  return NextResponse.next()
 })
 
 // Read more: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
