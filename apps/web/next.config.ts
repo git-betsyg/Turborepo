9ab6e23aac819ef5cfa8eeb88f271e797f977980
chenminggui <c13558889425@icloud.com>
2025-01-20 20:48:10 +0800

    feat(package.json): 引入MDX

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,18 +1,31 @@
 import type { NextConfig } from "next";
-import withBundleAnalyzer from '@next/bundle-analyzer'
+import withBundleAnalyzer from "@next/bundle-analyzer";
+import createMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
-    /* config options here */
-    // 优化包导入
-    experimental: {
-        optimizePackageImports: [],
-    },
-    // 选择特定软件包不捆绑
-    serverExternalPackages: [],
+  /* config options here */
+  // 优化包导入
+  experimental: {
+    optimizePackageImports: [],
+  },
+  // 选择特定软件包不捆绑
+  serverExternalPackages: [],
+  // Configure `pageExtensions` to include markdown and MDX files
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
-const withBundleAnalyzerConfig = withBundleAnalyzer({
-    enabled: process.env.ANALYZE === 'true',
-})
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+  options: {
+    // @ts-expect-error
+    remarkPlugins: ["remark-gfm"],
+    // @ts-expect-error
+    rehypePlugins: [["rehype-katex", { strict: true, throwOnError: true }]],
+  },
+});
 
-export default withBundleAnalyzerConfig(nextConfig)
+const withBundleAnalyzerConfig = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
+export default withBundleAnalyzerConfig(withMDX(nextConfig));
